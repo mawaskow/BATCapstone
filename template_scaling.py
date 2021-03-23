161e483a24288f35b39c4a07f98a3dbd1f88b6df
mawaskow <61253606+mawaskow@users.noreply.github.com>
2021-03-23 11:18:18 -0700

    initial commit

---
+++ template_scaling.py
@@ -0,0 +1,93 @@
+'''
+@mawaskow
+Description:
+This program allows a template to be scaled as it tries to match
+with image features in an algorithm.
+
+References:
+https://www.pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/
+https://docs.opencv.org/master/d4/dc6/tutorial_py_template_matching.html
+'''
+
+import numpy as np
+import argparse
+import imutils
+import glob
+import cv2
+
+'''
+ImagenTotal = np.asarray(Image.open('../Tutorial/Tozeur/Chabbat.png'))
+trees = ImagenTotal[:,:,0]
+
+ImagenTemplateSmall = np.asarray(Image.open('../Tutorial/Tozeur/Template1.png'))
+ImagenTemplateMedium = np.asarray(Image.open('../Tutorial/Tozeur/Template2.png'))
+ImagenTemplateLarge = np.asarray(Image.open('../Tutorial/Tozeur/Template3.png'))
+ImagenTemplateExtra = np.asarray(Image.open('../Tutorial/Tozeur/Template4.png'))
+'''
+
+# construct the argument parser and parse the arguments
+print("Parsing arguments...")
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--template", required=True, help='../Tutorial/Tozeur/Template1.png')
+ap.add_argument("-i", "--images", required=True, help='../Tutorial/Tozeur/Chabbat.png')
+ap.add_argument("-v", "--visualize", help="Flag indicating whether or not to visualize each iteration")
+args = vars(ap.parse_args())
+
+# load the image image, convert it to grayscale, and detect edges
+print("Loading image...")
+template = cv2.imread(args["template"])
+
+print("Converting image to grayscale...")
+template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+print("Detecting edges...")
+template = cv2.Canny(template, 50, 200)
+(tH, tW) = template.shape[:2]
+cv2.imshow("Template", template)
+
+print("Searching for template matches...")
+# loop over the images to find the template in
+for imagePath in glob.glob(args["images"] + "/*.jpg"):
+	# load the image, convert it to grayscale, and initialize the
+	# bookkeeping variable to keep track of the matched region
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	found = None
+	# loop over the scales of the image
+	for scale in np.linspace(0.2, 1.0, 20)[::-1]:
+		# resize the image according to the scale, and keep track
+		# of the ratio of the resizing
+		resized = imutils.resize(gray, width = int(gray.shape[1] * scale))
+		r = gray.shape[1] / float(resized.shape[1])
+		# if the resized image is smaller than the template, then break
+		# from the loop
+		if resized.shape[0] < tH or resized.shape[1] < tW:
+			break
+        # detect edges in the resized, grayscale image and apply template
+		# matching to find the template in the image
+		edged = cv2.Canny(resized, 50, 200)
+		result = cv2.matchTemplate(edged, template, cv2.TM_CCOEFF)
+		(_, maxVal, _, maxLoc) = cv2.minMaxLoc(result)
+		# check to see if the iteration should be visualized
+		if args.get("visualize", False):
+			# draw a bounding box around the detected region
+			clone = np.dstack([edged, edged, edged])
+			cv2.rectangle(clone, (maxLoc[0], maxLoc[1]),
+				(maxLoc[0] + tW, maxLoc[1] + tH), (0, 0, 255), 2)
+			cv2.imshow("Visualize", clone)
+			cv2.waitKey(0)
+		# if we have found a new maximum correlation value, then update
+		# the bookkeeping variable
+		if found is None or maxVal > found[0]:
+			found = (maxVal, maxLoc, r)
+	# unpack the bookkeeping variable and compute the (x, y) coordinates
+	# of the bounding box based on the resized ratio
+	(_, maxLoc, r) = found
+	(startX, startY) = (int(maxLoc[0] * r), int(maxLoc[1] * r))
+	(endX, endY) = (int((maxLoc[0] + tW) * r), int((maxLoc[1] + tH) * r))
+	# draw a bounding box around the detected result and display the image
+	cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+	cv2.imshow("Image", image)
+	cv2.waitKey(0)
+
+print("Program ended.")
