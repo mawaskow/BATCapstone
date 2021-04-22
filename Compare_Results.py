0dadca05a3b350ad2f14e5ea1bbc0bacf8b3c481
vllindsey <70986053+vllindsey@users.noreply.github.com>
2021-04-22 16:55:48 -0700

    Add files via upload

---
+++ Compare_Results.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 22 15:40:06 2021
+
+@author: Victoria Lidnsey 
+"""
+template <- #input the results from Ales program
+segmentation <- #input the results from Victoria program
+
+print("What is the estimate plants in the image?")
+print("Remember to press enter after number is inputed.")
+estimate = int(input(" estimate:"))
+
