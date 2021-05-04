3818e9781ec4e540c1f2f3e72039e8570016f8f8
mawaskow <61253606+mawaskow@users.noreply.github.com>
2021-05-03 20:35:22 -0700

    code integration continued

--- capstonefinal.py
+++ capstonefinal.py
@@ -41,11 +41,15 @@ import imutils
 import glob
 import cv2
 
-##### 
+##### function definitions
 
 def gettestimg(show = False):
     '''
-    Returns the test image to be analyzed.
+    Author: Waskow
+    Purpose: Gets the hardcoded Chabbat image
+    Parameters:
+        show = [boolean] whether or not to display the images
+    Returns: Reference image (PIL Image.open() type)
     '''
     img = Image.open('../Tutorial/Tozeur/Chabbat.png')
     ImagenTotal = np.asarray(img)
@@ -58,8 +62,11 @@ def gettestimg(show = False):
 
 def get_img(show = False):
     '''
-    Prompts user to select the file they want to open.
-    Returns the image to be analyzed.
+    Author: Waskow
+    Purpose: Gets user-selected image (both reference and templates)
+    Parameters: 
+        show = [boolean] whether or not to display the old & new images
+    Returns: Selected image (PIL Image.open() type)
     '''
     infile= askopenfilename()
     print(infile)
@@ -72,23 +79,28 @@ def get_img(show = False):
         plt.show()
     return img
 
-###
-
 def line_select_callback(eclick, erelease):
     '''
-    Callback for line selection.
-
-    *eclick* and *erelease* are the press and release events.
-    https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
+    Author: Matplotlib reference
+        https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
+    Purpose: Callback for line selection.
+    Parameters: 
+        eclick = press event
+        erelease = release event
+    Returns: None
     '''
     x1, y1 = eclick.xdata, eclick.ydata
     x2, y2 = erelease.xdata, erelease.ydata
     print(f"({x1:3.2f}, {y1:3.2f}) --> ({x2:3.2f}, {y2:3.2f})")
-    #tmp.append([x1, y1, x2, y2])
 
 def toggle_selector(event):
     '''
-    https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
+    Author: Matplotlib reference
+        https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
+    Purpose: Allows selector to be activated/deactivated
+    Parameters: 
+        event = key-press/click event
+    Returns: None
     '''
     print(' Key pressed.')
     if event.key == 't':
@@ -101,7 +113,12 @@ def toggle_selector(event):
 
 def select_temp(img):
     '''
-    https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
+    Author: Waskow
+    Purpose: Allows selector to be activated/deactivated
+    Parameters: 
+        img = image (opened with PIL library Image.open() method)
+    Returns: None
+    Notes: RectangleSelector call from https://matplotlib.org/stable/gallery/widgets/rectangle_selector.html
     '''
     fig, ax = plt.subplots(figsize = (10,7))
     N = 100000  # If N is large one can see improvement by using blitting.
@@ -125,11 +142,11 @@ def adj_contrast(img, clvl, show = False):
     '''
     Author: Waskow
     Purpose: Adjusts contrast to enhance & clean image
-    Inputs: 
+    Parameters: 
         img = image (opened with PIL library Image.open() method)
         clvl = [integer]
         show = [boolean] whether or not to display the old & new images
-    Returns: contrast-adjusted image
+    Returns: contrast-adjusted image (image type)
     '''
     enhancer = ImageEnhance.Contrast(img)
     factor = 259*(clvl+255)/(255*(259-clvl))
@@ -142,94 +159,114 @@ def adj_contrast(img, clvl, show = False):
         ax[1].imshow(img_cont)
     return img_cont
 
-def segmentation(filename):
+def segmentation(img):
     '''
     Author: Lindsey
     Purpose: Completes segmentation method
-    Inputs: filename
+    Parameters:
+        img = image (opened with PIL library Image.open() method)
     Returns: 
+        segmentation = 
+        labeled_trees = 
     '''
-    Image_1 = np.asarray(Image.open(filename))
+    Image_1 = np.asarray(img)
     tree = Image_1[:, :, 0]
-
     for i in range(0, 3):
         hist, hist_centers = histogram(Image_1[:,:,i])
+        plt.figure(figsize = (10,10))
         plt.plot(hist_centers, hist)
         plt.xlabel('Value')
         plt.ylabel('Frequency')
         plt.title('Histogram of Image Values')
         plt.show()
-
-    print("After looking at the histogram detemine the upper and the lower setting for the markers.")
+    print("\nAfter looking at the histogram detemine the upper and the lower setting for the markers.")
     print("Press enter when finished")
-    lower = int(input("Lower Bound:"))
-    upper = int(input("Upper Bound:"))
-
+    lower = int(input("\nLower Bound: "))
+    upper = int(input("\nUpper Bound: "))
     if lower >250 or lower < 0:
-        print("Unusable input for Lower Bound would you like to retry?")
-        response = input("Input Y for yes and N for no:")
+        response = input("\nUnusable input for Lower Bound. \nWould you like to retry? (Y/N): ")
         if response == "Y":
-            lower = int(input("Lower Bound:")) 
-    
-    print("")       
- 
+            lower = int(input("\nLower Bound: ")) 
     if upper > 250 or upper < 0:
-        print("Unusable input for Upper Bound would you like to retry?")
-        response = input("Input Y for yes and N for no:")
+        response = input("\nUnusable input for Upper Bound. \nWould you like to retry? (Y/N): ")
         if response == "Y":
-            upper = int(input("Upper Bound:"))                      
-
-
+            upper = int(input("\nUpper Bound: "))                      
+    
     edge = canny(tree/600)
     fill_tree = ndi.binary_fill_holes(edge)
-    plt.imshow(fill_tree)
-
     label_objects, nb_labels = ndi.label(fill_tree)
     sizes = np.bincount(label_objects.ravel())
     mask_sizes = sizes > 5
     mask_sizes[0] = 0
-    trees_cleaned = mask_sizes[label_objects] 
-    plt.imshow(trees_cleaned)
+    trees_cleaned = mask_sizes[label_objects]
+    
+    plt.figure(figsize = (10,10))
+    plt.imshow(fill_tree)
+    plt.title("Fill tree")
+    plt.show()
 
-    elevation_map = sobel(Image_1)
+    plt.figure(figsize = (10,10))
+    plt.imshow(trees_cleaned)
+    plt.title("Trees Cleaned")
+    plt.show()
+
+    plt.figure(figsize = (10,10))
     plt.imshow(elevation_map)
-    markers = np.zeros_like(Image_1)
-    markers[Image_1 < lower] = 1
-    markers[Image_1 > upper] = 2
+    plt.show()
 
     #Chabbat: upper = 180, lower = 110
     #PalmTreesOasis1: upper = 80, lower = 100
     #Shakmo: upper = 100, lower = 75
 
+    elevation_map = sobel(Image_1)
+    markers = np.zeros_like(Image_1)
+    markers[Image_1 < lower] = 1
+    markers[Image_1 > upper] = 2
     segmentation = watershed(elevation_map, markers)
     segmentation = ndi.binary_fill_holes(segmentation - 1)
     labeled_trees, _ = ndi.label(segmentation)
-    plt.imshow(segmentation[:,:,0])
-    
-    return(segmentation, labeled_trees)
 
-def categorization(filename, threshold = 0.7):
+    plt.figure(figsize = (10,10))
+    plt.imshow(segmentation[:,:,0])
+    plt.show()
+
+    print('segmentation')
+    print(segmentation)
+    print("labeled_trees")
+    print(labeled_trees)
+
+    return segmentation, labeled_trees
+
+def categorization(img, threshold = 0.7):
     '''
-    Author: 
+    Author: Mpoyi
     Purpose: 
-    Inputs:
+    Parameters:
+        filename
+        threshold = [float] template matching threshold between 0 and 1
     Returns:
     '''
-    img_rgb = cv2.imread(filename)  # original image
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY) # grey-scaling original image
-
-    template_small = cv2.imread(category_small,0) #reading template
+    # original image
+    #img_rgb = cv2.imread(img)
+    img_rgb = img
+    # grey-scaling original image
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    #reading template
+    template_small = cv2.imread(category_small,0)
     w, h = template_small.shape[::-1] 
 
-    res_small = cv2.matchTemplate(img_gray,template_small,cv2.TM_CCOEFF_NORMED) 
-    count_small = 0  # creating counter for the matched trees
+    res_small = cv2.matchTemplate(img_gray,template_small,cv2.TM_CCOEFF_NORMED)
+    # creating counter for the matched trees
+    count_small = 0
     loc = np.where( res_small >= threshold)
     for pt in zip(*loc[::-1]):
-        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)# creating rectangle of region of template
+        # creating rectangle of region of template
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
         count_small+=1
-    cv2.imwrite('res_small.png',img_rgb) # creating output file of counted trees being part of that category
-
-    template_medium = cv2.imread(category_medium,0) #reading template
+    # creating output file of counted trees being part of that category
+    cv2.imwrite('res_small.png',img_rgb)
+    #reading template
+    template_medium = cv2.imread(category_medium,0)
     w, h = template_medium.shape[::-1] 
 
     res_medium = cv2.matchTemplate(img_gray,template_medium,cv2.TM_CCOEFF_NORMED) 
@@ -241,8 +278,8 @@ def categorization(filename, threshold = 0.7):
         count_mid+=1
     
     cv2.imwrite('res_medium.png',img_rgb)
-
-    template_large = cv2.imread(category_large,0) #reading template
+    #reading template
+    template_large = cv2.imread(category_large,0)
     w, h = template_large.shape[::-1] 
 
     res_large = cv2.matchTemplate(img_gray,template_large,cv2.TM_CCOEFF_NORMED) 
@@ -253,50 +290,48 @@ def categorization(filename, threshold = 0.7):
         count_large+=1
     
     cv2.imwrite('res_large.png',img_rgb)
-
-    Total = count_small + count_mid + count_large # counting total number of trees categorized
+    # counting total number of trees categorized
+    Total = count_small + count_mid + count_large
 
     return [count_small, count_mid, count_large, Total]
 
 def compare(temp_list, seg_list):
     '''
-    Author: 
+    Author: Lindsey
     Purpose: 
-    Inputs:
+    Parameters:
+        temp_list = the results from template-matching
+        seg_list = the results from segmentation
     Returns:
     '''
-    #template <- #input the results from Ales program
-    #segmentation <- #input the results from Victoria program
     template = temp_list[-1]
     segmentation = seg_list[-1]
-    print("The Template Matching program counted ", template, " trees in the image.")
-    print("The Segmentation program counted ", segmentation, " trees in the image.")
-    print("")
+    print("\nThe Template Matching program counted", template, "trees in the image.")
+    print("The Segmentation program counted", segmentation, "trees in the image.")
 
-    print("Is the number of trees known?")
-    known = input("Input Y for yes and N for no:")
-    print("")
+    print("\nIs the number of trees known?")
+    known = input("Input Y for yes and N for no: ")
 
     if known == "Y":
         difference_S = sqrt((known - segmentation)^2) 
         difference_T = sqrt((known - template)^2)
         if difference_T == 0:
-            print("Template Matching has the correct result.")
+            print("\nTemplate Matching has the correct result.")
         if difference_S == 0:
-            print("Segmantation has the correct result.")
+            print("\nSegmantation has the correct result.")
         if difference_T > difference_S:
-            print("Segmenation has the more accurate count.")
+            print("\nSegmenation has the more accurate count.")
             difference = known - segmantation
-            print("Segmentation was", difference, "trees off to actual amount.")
+            print("\nSegmentation was", difference, "trees off to actual amount.")
         if difference_T < difference_S:
-            print("Template Matching has the more accurate count.")
+            print("\nTemplate Matching has the more accurate count.")
             difference = known - template
-            print("Template Matching was", difference, "trees off to actual amount.")
+            print("\nTemplate Matching was", difference, "trees off to actual amount.")
 
-    if known == "N"
-        print("What is the estimate trees in the image?")
-        print("Remember to press enter after number is inputed.")
-        estimate = int(input(" estimate:"))
+    if known == "N":
+        print("\nWhat is the estimate trees in the image?")
+        print("Press enter after number is inputed.")
+        estimate = int(input("\nEstimate: "))
         est_dif_S = sqrt((estimate - segmentation)^2)
         est_dif_T = sqrt((estimate - templat)^2)
         if est_dif_S == 0:
@@ -316,24 +351,26 @@ def error_check(filename):
     '''
     Author: 
     Purpose: 
-    Inputs:
+    Parameters:
     Returns:
     '''
     image = np.asarray(Image.open(filename))
 
     elevation_map = sobel(image)
-    markers = np.zeros_like(image)  
-    markers[image < 110] = 1 #lower marker for Chabbat
-    markers[image > 100] = 2 #higher marker for Chabbat
+    markers = np.zeros_like(image)
+    # lower marker for Chabbat
+    markers[image < 110] = 1
+    # higher marker for Chabbat
+    markers[image > 100] = 2
     im_true = watershed(elevation_map, markers)
     im_true = ndi.label(ndi.binary_fill_holes(im_true - 1))[0]
 
     edges = sobel(image)
     im_test1 = watershed(edges, markers=468, compactness=0.001)
 
-    #image = img_as_float(image)
-
-    method_names = ['Compact watershed'] #segmentation method used in ur program
+    # image = img_as_float(image)
+    # segmentation method used in ur program
+    method_names = ['Compact watershed']
     short_method_names = ['Compact WS']
 
     precision_list = []
@@ -354,7 +391,7 @@ def error_check(filename):
         print(f"False Splits: {splits}")
         print(f"False Merges: {merges}")
 
-    fig, axes = plt.subplots(2, 3, figsize=(9, 6), constrained_layout=True)
+    fig, axes = plt.subplots(1, 2, figsize=(9, 6), constrained_layout=True)
     ax = axes.ravel()
 
     ax[0].scatter(merge_list, split_list)
@@ -376,31 +413,25 @@ def error_check(filename):
     ax[1].set_xlim(0, 1)
     ax[1].set_ylim(0, 1)
 
-    # cannot do this part yet because of a Value Error: NumPy boolean array indexing assignment cannot assign 3 input values to the 953328 output values where the mask is true
-    #ax[2].imshow(mark_boundaries(image, im_true)) 
-    #ax[2].set_title('True Segmentation')
-    #ax[2].set_axis_off()
-    #ax[3].imshow(mark_boundaries(image, im_test1))
-    #ax[3].set_title('Compact Watershed')
-    #ax[3].set_axis_off()
-
     plt.show()
 
 def main():
     '''
     Author: Waskow
-    Purpose: 
-    Inputs:
-    Returns:
+    Purpose: To run the algorithm
+    Parameters: None
+    Returns: None
     '''
+    # print current path
     path = os.getcwd()
-    print(path)
-    img = Image.open('../Tutorial/Tozeur/Chabbat.png')
+    print("\nCurrent path:", path)
+    # HARDCODED
+    img = gettestimg(show = False)
     print("Enhancing image...")
     contrast = 60
     adj_img = adj_contrast(img, contrast, show = True)
     print("categorizing")
-    categorization(filename)
+    categorization(img, threshold = 0.7)
     # need to adapt to selected templates/ undo hardcoding
     print("Compiling results...")
     see_results = False
